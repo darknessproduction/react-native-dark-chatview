18b2728bfc16cc88f9dc8a284c0393fadfacf751
darq(@) <ilya@sarzhevsky.com>
2019-09-23 05:08:15 +0300

    Update idx.js

--- idx.js
+++ idx.js
@@ -1,11 +1,3 @@
-/**
- * Author: Shi(bolan0000@icloud.com)
- * Date: 2018/7/4
- * Copyright (c) 2018, AoTang, Inc.
- *
- * Description:
- */
-
 export function idx<T>(f: () => T, defaultValue?: T | string) {
   try {
     const res = f();
